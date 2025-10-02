e2296025312e262547ba7181a54651f71e1a3ae7
Pasindu B. Dimantha <pasindubhagya2001@gmail.com>
2025-10-03 01:34:56 +0530

    settings finish2

---
+++ app/src/main/java/com/example/dailywellnessapp/ui/onboarding/OnboardingOneActivity.kt
@@ -0,0 +1,28 @@
+package com.example.dailywellnessapp.ui.onboarding
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.dailywellnessapp.MainActivity
+import com.example.dailywellnessapp.R
+
+class OnboardingOneActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding_one)
+
+        val nextButton = findViewById<Button>(R.id.next_button)
+        nextButton.setOnClickListener {
+            startActivity(Intent(this, OnboardingTwoActivity::class.java))
+            finish()
+        }
+
+        val skipText = findViewById<TextView>(R.id.skip_text)
+        skipText.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+}
